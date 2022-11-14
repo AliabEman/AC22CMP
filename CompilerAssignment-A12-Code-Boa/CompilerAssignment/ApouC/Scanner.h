58286abdd114c1fba2f4f689a5ac4ecbf48554bc
AliabEman <63355970+AliabEman@users.noreply.github.com>
2022-11-13 23:59:23 -0500

    A22
    Focal point was on fixing the broken reader, still not completely fixed and the scanner is unimplemented

---
+++ CompilerAssignment-A12-Code-Boa/CompilerAssignment/ApouC/Scanner.h
@@ -0,0 +1,264 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Winter, 2022
+* Author: Svillen Ranev - Paulo Sousa
+* Professors: Paulo Sousa
+************************************************************
+*/
+
+/*
+************************************************************
+* File name: scanner.h
+* Compiler: MS Visual Studio 2022
+* Author: Paulo Sousa
+* Course: CST 8152 – Compilers, Lab Section: [011, 012, 013]
+* Assignment: A22, A32.
+* Date: Jul 01 2022
+* Purpose: This file is the main header for Scanner (.h)
+* Function list: (...).
+*************************************************************/
+
+#ifndef COMPILERS_H_
+#include "Compilers.h"
+#endif
+
+#ifndef SCANNER_H_
+#define SCANNER_H_
+
+#ifndef NULL
+#include <_null.h> /* NULL pointer constant is defined there */
+#endif
+
+/*#pragma warning(1:4001) */	/*to enforce C89 type comments  - to make //comments an warning */
+
+/*#pragma warning(error:4001)*/	/* to enforce C89 comments - to make // comments an error */
+
+/* Constants */
+#define VID_LEN 20  /* variable identifier length */
+#define ERR_LEN 40  /* error message length */
+#define NUM_LEN 5   /* maximum number of digits for IL */
+
+#define RTE_CODE 1  /* Value for run-time error */
+
+/* TO_DO: Define Token codes - Create your token classes */
+enum TOKENS {
+	ERR_T,		/*  0: Error token */
+	MNID_T,		/*  1: Method name identifier token (start: &) */
+	STR_T,		/*  2: String literal token */
+	LPR_T,		/*  3: Left parenthesis token */
+	RPR_T,		/*  4: Right parenthesis token */
+	LBR_T,		/*  5: Left brace token */
+	RBR_T,		/*  6: Right brace token */
+	KW_T,		/*  7: Keyword token */
+	EOS_T,		/*  8: End of statement (semicolon) */
+	RTE_T,		/*  9: Run-time error token */
+	INL_T,		/* 10: Run-time error token */
+	SEOF_T		/* 11: Source end-of-file token */
+
+	MNID_T, /* Method Name Identifier Token */
+	KEY_T, /* Keyword Token*/
+	SL_T, /*String Literal */
+	CL_T, /*Character Literal */
+	FPL_T, /*Floating Point Literal */
+	IL_T, /*Integer Literal */
+	MLC_T, /*Multi-Line Comment Token*/
+	SLC /*Single-Line Comment Token*/
+
+};
+
+/* TO_DO: Operators token attributes */
+typedef enum ArithmeticOperators { OP_ADD, OP_SUB, OP_MUL, OP_DIV } AriOperator;
+typedef enum RelationalOperators { OP_EQ, OP_NE, OP_GT, OP_LT } RelOperator;
+typedef enum LogicalOperators { OP_AND, OP_OR, OP_NOT } LogOperator;
+typedef enum SourceEndOfFile { SEOF_0, SEOF_255 } EofOperator;
+
+/* TO_DO: Data structures for declaring the token and its attributes */
+typedef union TokenAttribute {
+	apc_intg codeType;      /* integer attributes accessor */
+	AriOperator arithmeticOperator;		/* arithmetic operator attribute code */
+	RelOperator relationalOperator;		/* relational operator attribute code */
+	LogOperator logicalOperator;		/* logical operator attribute code */
+	EofOperator seofType;				/* source-end-of-file attribute code */
+	apc_intg intValue;						/* integer literal attribute (value) */
+	apc_intg keywordIndex;					/* keyword index in the keyword table */
+	apc_intg contentString;				/* string literal offset from the beginning of the string literal buffer (stringLiteralTable->content) */
+	apc_real floatValue;					/* floating-point literal attribute (value) */
+	apc_char idLexeme[VID_LEN + 1];		/* variable identifier token attribute */
+	apc_char errLexeme[ERR_LEN + 1];		/* error token attribite */
+} TokenAttribute;
+
+/* TO_DO: Should be used if no symbol table is implemented */
+typedef struct idAttibutes {
+	apc_byte flags;			/* Flags information */
+	union {
+		apc_intg intValue;				/* Integer value */
+		apc_real floatValue;			/* Float value */
+		apc_char* stringContent;		/* String value */
+	} values;
+} IdAttibutes;
+
+/* Token declaration */
+typedef struct Token {
+	apc_intg code;				/* token code */
+	TokenAttribute attribute;	/* token attribute */
+	IdAttibutes   idAttribute;	/* not used in this scanner implementation - for further use */
+} Token;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* EOF definitions */
+#define CHARSEOF0 '\0'
+#define CHARSEOF255 0xFF
+
+/*  Special case tokens processed separately one by one in the token-driven part of the scanner
+ *  '=' , ' ' , '(' , ')' , '{' , '}' , == , <> , '>' , '<' , ';', 
+ *  white space, #comment\n , ',' , ';' , '-' , '+' , '*' , '/', # , 
+ *  .&., .|. , .!. , SEOF.
+ */
+
+/* TO_DO: Define lexeme FIXED classes */
+/* These constants will be used on nextClass */
+#define CHRCOL2 '_' 
+#define CHRCOL3 '&' 
+#define CHRCOL4 '\''
+#define LETTER [A,z] /*Letter*/
+#define DIGIT [0,9] /* Digit */
+#define SQ 27 /*Single Line Delimitator - Single Quote for chars*/
+#define Q 34 /*Single Line Delimitator - Double Quote for string*/
+#define MLC 57 /* Multiline Comment slash*/
+#define STAR 52 /* Multiline comment star*/
+#define SLC '#' /*Single line Comment Hash*/
+#define P '.' /*Period for Floating Point Numbers*/
+#define E 'e' /*Exponential for Scientific Notation*/
+#define SP '+' /*Positive sign for Scientific Notation*/
+#define SN '-' /*Negative sign for Scientific Notation*/
+#define U '_' /*Underscore*/
+#define OPENP '(' /*Open parenthesis for MNID*/
+#define CLOSP ')' /*Close parenthesis for MNID*/
+#define OTHER /*Not Finished*/
+
+/* These constants will be used on VID / MID function */
+#define MNIDPREFIX '&'
+
+/* TO_DO: Error states and illegal state */
+#define FS		100		/* Illegal state */
+#define ESWR	101		/* Error state with retract */
+#define ESNR	102		/* Error state with no retract */
+
+ /* TO_DO: State transition table definition */
+#define TABLE_COLUMNS 7
+
+/* TO_DO: Transition table - type of states defined in separate table */
+static apc_intg transitionTable[][TABLE_COLUMNS] = {
+/*   [A-z] , [0-9],    _,    &,    ", SEOF, other
+	   L(0),  D(1), U(2), M(3), Q(4), E(5),  O(6) */
+	{     1,  ESNR, ESNR, ESNR,    4, ESWR, ESNR}, // S0: NOAS
+	{     1,     1,    1,    2, ESNR, ESWR,    3}, // S1: NOAS
+	{    FS,    FS,   FS,   FS,   FS,   FS,   FS}, // S2: ASNR (MVID)
+	{    FS,    FS,   FS,   FS,   FS,   FS,   FS}, // S3: ASWR (KEY)
+	{     4,     4,    4,    4,    5, ESWR,    4}, // S4: NOAS
+	{    FS,    FS,   FS,   FS,   FS,   FS,   FS}, // S5: ASNR (SL)
+	{    FS,    FS,   FS,   FS,   FS,   FS,   FS}, // S6: ASNR (ES)
+	{    FS,    FS,   FS,   FS,   FS,   FS,   FS}  // S7: ASWR (ER)
+};
+
+/* Define accepting states types */
+#define NOFS	0		/* not accepting state */
+#define FSNR	1		/* accepting state with no retract */
+#define FSWR	2		/* accepting state with retract */
+
+/* TO_DO: Define list of acceptable states */
+static apc_intg stateType[] = {
+	NOFS, /* 00 */
+	NOFS, /* 01 */
+	FSNR, /* 02 (MID) - Methods */
+	FSWR, /* 03 (KEY) */
+	NOFS, /* 04 */
+	FSNR, /* 05 (SL) */
+	FSNR, /* 06 (Err1 - no retract) */
+	FSWR  /* 07 (Err2 - retract) */
+};
+
+/*
+-------------------------------------------------
+TO_DO: Adjust your functions'definitions
+-------------------------------------------------
+*/
+
+/* Static (local) function  prototypes */
+apc_intg startScanner(ReaderPointer psc_buf);
+static apc_intg nextClass(apc_char c);			/* character class function */
+static apc_intg nextState(apc_intg, apc_char);		/* state machine function */
+
+/*
+-------------------------------------------------
+Automata definitions
+-------------------------------------------------
+*/
+
+/* TO_DO: Pointer to function (of one char * argument) returning Token */
+typedef Token(*PTR_ACCFUN)(apc_char* lexeme);
+
+/* Declare accepting states functions */
+Token funcSL	(apc_char lexeme[]);
+Token funcID	(apc_char lexeme[]);
+Token funcKEY	(apc_char lexeme[]);
+Token funcErr	(apc_char lexeme[]);
+
+/* 
+ * Accepting function (action) callback table (array) definition 
+ * If you do not want to use the typedef, the equvalent declaration is:
+ */
+
+/* TO_DO: Define final state table */
+static PTR_ACCFUN finalStateTable[] = {
+	NULL,		/* -    [00] */
+	NULL,		/* -    [01] */
+	funcID,		/* MNID	[02] */
+	funcKEY,	/* KEY  [03] */
+	NULL,		/* -    [04] */
+	funcSL,		/* SL   [05] */
+	funcErr,	/* ERR1 [06] */
+	funcErr		/* ERR2 [07] */
+};
+
+/*
+-------------------------------------------------
+Language keywords
+-------------------------------------------------
+*/
+
+/* TO_DO: Define the number of Keywords from the language */
+#define KWT_SIZE 10
+
+/* TO_DO: Define the list of keywords */
+static apc_char* keywordTable[KWT_SIZE] = {
+	"data",
+	"code",
+	"int",
+	"real",
+	"string",
+	"if",
+	"then",
+	"else",
+	"while",
+	"do"
+};
+
+/* NEW SECTION: About indentation */
+
+/*
+ * Scanner attributes to be used (ex: including: intendation data
+ */
+
+#define INDENT '\t'  /* Tabulation */
+
+/* TO_DO: Should be used if no symbol table is implemented */
+typedef struct languageAttributes {
+	apc_char indentationCharType;
+	apc_intg indentationCurrentPos;
+	/* TO_DO: Include any extra attribute to be used in your scanner (OPTIONAL and FREE) */
+} LanguageAttributes;
+
+#endif
